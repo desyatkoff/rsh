9779cf1e18c9737fcb7c99445c11418bfb719f45
Desyatkov Sergey <sergeidesyatkov@gmail.com>
2025-04-18 19:20:52 +0500

    ADD: Auto-install script

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+
+###########################################
+#                                         #
+#  INSTALLER SCRIPT FOR RSH (RUST SHELL)  #
+#                                         #
+###########################################
+
+
+# 0. Before installation
+
+# Warning!
+# You can only continue if you already have Rust and Cargo installed
+
+
+# 1. Compile the Rust project
+
+cargo build --release
+
+
+# 2. Copy Rsh binary file to the `/bin/` directory
+
+cp -v \
+    ./target/release/rsh \
+    /bin/
+
+
+# 3. Launch Rsh
+
+rsh
+
+
+# 4. After installation
+
+# Success!
+# Enjoy your new blazingly fast and very unuseful trashy shell
+

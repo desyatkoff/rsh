34445b5397474d897c869aa0eb0b3f23ba172cdb
Desyatkov Sergey <sergeidesyatkov@gmail.com>
2025-04-18 18:35:40 +0500

    UPDATE: Get user input, parse args and execute

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io,
+    io::Write,
+    process::Command
+};
+
+fn get_input(query: &str) -> String {
+    let mut input = String::new();
+
+    print!("{}", query);
+
+    io::stdout().flush();
+    io::stdin()
+        .read_line(&mut input)
+        .unwrap();
+
+    return input
+        .trim()
+        .to_owned();
 }
+
+fn main() {
+    loop {
+        let input = get_input("> ");
+        let mut input_parts = input
+            .trim()
+            .split_whitespace();
+        let cmd = input_parts
+            .next()
+            .unwrap();
+
+        Command::new(cmd)
+            .args(input_parts)
+            .spawn()
+            .unwrap()
+            .wait();
+    }
+}
+
